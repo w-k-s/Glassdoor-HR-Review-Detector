ca6c921bfebbfffc23bb3acbe408235251286ca8
Waqqas Sheikh <waqqas.abdulkareem@gmail.com>
2024-08-30 23:18:28 +0400

    Document theory and setup experiment to verify theory

---
+++ utils/utils.py
@@ -0,0 +1,17 @@
+import csv
+import io
+
+def load_csv_as_dictionary(csv_filename):
+    with open(csv_filename, mode='r', newline='') as file:
+        reader = csv.DictReader(file)
+        data = [row for row in reader]  
+    return data
+
+def array_of_dictionaries_to_csv_string(items):
+    with io.StringIO() as output:
+        writer = csv.DictWriter(output, fieldnames=items[0].keys())
+        writer.writeheader()
+        writer.writerows(items)
+        
+        result = output.getvalue()
+    return result
