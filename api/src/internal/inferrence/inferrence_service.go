ad5f04238385f26f092048af24412b20198f36d9
Waqqas Sheikh <waqqas.abdulkareem@gmail.com>
2024-10-12 22:35:18 +0400

    Cache genuity responses

--- api/src/internal/inferrence/inferrence_service.go
+++ api/src/internal/inferrence/inferrence_service.go
@@ -10,98 +10,121 @@ import (
 	"math"
 	"net/http"
 
+	"com.github/w-k-s/glassdoor-hr-review-detector/internal"
 	"com.github/w-k-s/glassdoor-hr-review-detector/pkg/services"
 	"com.github/w-k-s/glassdoor-hr-review-detector/pkg/types"
 )
 
 const vectorDimensions = 256
-const inputSize = 1 + (2*vectorDimensions)
+const inputSize = 1 + (2 * vectorDimensions)
+
+type predictionResponse struct {
+	Predictions [][]float64 `json:"predictions"`
+}
 
 type inferenceService struct {
 	embeddingService services.EmbeddingsService
+	cache            internal.Cache
 }
 
-type predictionResponse struct {
-    Predictions [][]float64 `json:"predictions"`
-}
-
-func MustInferenceService(embeddingService services.EmbeddingsService) services.InferenceService {
+func MustInferenceService(
+	embeddingService services.EmbeddingsService,
+	cache internal.Cache,
+) services.InferenceService {
 	if embeddingService == nil {
 		log.Panic("Embedding Service is nil")
 	}
-	
+
+	if cache == nil {
+		log.Panic("cache is nil")
+	}
+
 	return &inferenceService{
 		embeddingService: embeddingService,
+		cache:            cache,
 	}
 }
 
-func (svc inferenceService) GetGenuity(ctx context.Context,reviews []types.Review) ([]types.GenuityResult, error){
-	
-	embeddingRequestInputs := make([]string, len(reviews)*2)
-    for i, d := range reviews {
-        embeddingRequestInputs[i] = d.Pros
-		embeddingRequestInputs[i+1] = d.Cons
-		i += 2
-    }
-
-	embeddings,err := svc.embeddingService.GetEmbeddings(ctx, embeddingRequestInputs, vectorDimensions)
-	if err != nil{
-		return nil, err
+func (svc inferenceService) GetGenuity(ctx context.Context, reviews []types.Review) ([]types.GenuityResult, error) {
+	// TODO: Cache the review genuity by {reviewId}
+	genuityResults := make([]types.GenuityResult, 0)
+	newReviews := make([]types.Review, 0)
+	for _, review := range reviews {
+		if result, ok := svc.cache.Get(review.ID); ok {
+			genuityResults = append(genuityResults, result.(types.GenuityResult))
+		} else {
+			newReviews = append(newReviews, review)
+		}
 	}
 
-	x := make([][]float64, len(reviews))
-	for i, d := range reviews {
+	embeddingRequestInputs := make([]string, len(newReviews)*2)
+	for i, d := range newReviews {
+		embeddingRequestInputs[i] = d.Pros
+		embeddingRequestInputs[i+1] = d.Cons
+		i += 2
+	}
+
+	embeddings, err := svc.embeddingService.GetEmbeddings(ctx, embeddingRequestInputs, vectorDimensions)
+	if err != nil {
+		return nil, fmt.Errorf("embedding Service Error: %w", err)
+	}
+
+	x := make([][]float64, len(newReviews))
+	for i, d := range newReviews {
 		rating := d.Rating
 		prosEmbedding := embeddings[i*2]
 		consEmbedding := embeddings[1+(i*2)]
-		
+
 		consIndex := vectorDimensions + 1
 
 		x[i] = make([]float64, inputSize)
 		x[i][0] = rating
 		copy(x[i][1:consIndex], prosEmbedding)
 		copy(x[i][consIndex:], consEmbedding)
-    }
+	}
 
-	jsonData,_ := json.Marshal(struct {
+	jsonData, _ := json.Marshal(struct {
 		Instances [][]float64 `json:"instances"`
 	}{
 		Instances: x,
 	})
 
+	// TODO: Externalise API Endpoint
 	resp, err := http.Post("http://localhost:8501/v1/models/glassdoor_hr_review_detector:predict", "application/json", bytes.NewReader(jsonData))
-    if err != nil {
-        return nil, fmt.Errorf("inference API failed: %w", err)
-    }
-    defer resp.Body.Close()
+	if err != nil {
+		return nil, fmt.Errorf("inference API Error: %w", err)
+	}
+	defer resp.Body.Close()
 
 	body, err := io.ReadAll(resp.Body)
-    if err != nil {
-        return nil, fmt.Errorf("failed to read response body: %w", err)
-    }
-
-	log.Print(string(body))
-    var predResp predictionResponse
-    err = json.Unmarshal(body, &predResp)
-    if err != nil {
-        return nil, fmt.Errorf("failed to unmarshal response: %w", err)
-    }
-
-    predictions := make([]bool, len(predResp.Predictions))
-    for i, pred := range predResp.Predictions {
-        if len(pred) != 1 {
-            return nil, fmt.Errorf("unexpected prediction array length at index %d: got %d, want 1", i, len(pred))
-        }
-        predictions[i] = sigmoid(pred[0]) < 0.5
-    }
-
-	genuityResults := make([]types.GenuityResult, len(reviews))
-	for i, review := range reviews {
-		genuityResults[i].ReviewID = review.ID
-		genuityResults[i].IsGenuine = predictions[i]
+	if err != nil {
+		return nil, fmt.Errorf("failed to read inference response: %w", err)
 	}
 
-	return genuityResults,nil
+	var predResp predictionResponse
+	err = json.Unmarshal(body, &predResp)
+	if err != nil {
+		return nil, fmt.Errorf("failed to unmarshal inference response: %w", err)
+	}
+
+	predictions := make([]bool, len(predResp.Predictions))
+	for i, pred := range predResp.Predictions {
+		if len(pred) != 1 {
+			return nil, fmt.Errorf("unexpected prediction array length at index %d: got %d, want 1", i, len(pred))
+		}
+		predictions[i] = sigmoid(pred[0]) < 0.5
+	}
+
+	for i, newReview := range newReviews {
+		result := types.GenuityResult{
+			ReviewID:  newReview.ID,
+			IsGenuine: predictions[i],
+		}
+		svc.cache.Set(newReview.ID, result)
+		genuityResults = append(genuityResults, result)
+	}
+
+	return genuityResults, nil
 }
 
 func sigmoid(v float64) float64 {
